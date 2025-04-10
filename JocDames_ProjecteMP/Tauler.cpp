413210fc298da6e40180c15cf395961bf98965f2
NIU1749831 <1749831@uab.cat>
2025-04-10 20:09:34 +0200

    clase tauler i primera funcio(inicialitza tauler)

---
+++ JocDames_ProjecteMP/Tauler.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include "Tauler.h"
+
+using namespace std;
+
+void Tauler::inicialitza(const string& nomFitxer)
+{
+    //Abro el fichero que nos han pasado como parametros
+    ifstream ficher(nomFitxer);
+    if (ficher.is_open())
+    {
+        //Inicializo primero todas las casillas como empty 
+        for (int i = 0; i < N_FILES; i++)
+        {
+            for (int j = 0; j < N_COLUMNES; j++)
+            {
+                m_tauler [i][j].Fitxa(TIPUS_EMPTY, i, j);
+            }
+            
+        }
+        
+        //Ahora extraigo los datos de el fichero linea por linea y modifico sus respectivos atributos segun los datos leidos
+        char tipus;
+        char x;
+        int y;
+        while (ficher >> tipus >> x >> y)
+        {
+            int Y = y -1;
+            int X = x - 'a';
+            switch (tipus)
+            {
+            case 'O':
+                m_tauler [X][Y].setTipus(TIPUS_NORMAL);
+                m_tauler [X][Y].setColor(COLOR_BLANC);
+                break;
+            case 'X':
+                m_tauler [X][Y].setTipus(TIPUS_NORMAL);
+                m_tauler [X][Y].setColor(COLOR_NEGRE);
+                break;
+            case 'D':
+                m_tauler [X][Y].setTipus(TIPUS_DAMA);
+                m_tauler [X][Y].setColor(COLOR_BLANC);
+                break;
+            case 'R':
+                m_tauler [X][Y].setTipus(TIPUS_DAMA);
+                m_tauler [X][Y].setColor(COLOR_NEGRE);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+void Tauler::actualitzaMovimentsValids()
+{
+    
+}
---
+++ JocDames_ProjecteMP/Tauler.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Tauler.cpp"
+#include "fitxa.h"
+#include "posicio.h"
+#include "moviment.h"
+
+using namespace std;
+
+#define N_FILES = 8;
+#define N_COLUMNES = 8;
+
+
+class Tauler
+{
+public:
+
+private:
+    Fitxa m_tauler[N_FILES][N_COLUMNES];
+    void inicialitza(const string& nomFitxer);
+    void actualitzaMovimentsValids();
+    bool mouFitxa(const Posicio& origen, const Posicio& desti);
+    void getPosicionsPossibles(cont Posicio& origen, 
+    int& nPosicions, Posicio posicionsPossibles[]);
+    string toString() const;
+}
+
