21d28b44c9a27fa0f69972318a63c6238ef2e0a8
Oriol <1621578@uab.cat>
2025-04-28 19:32:32 +0200

    implementacio getters/setters/construcotrs classes: fitxa, moviment

--- JocDames_ProjecteMP/Moviment.cpp
+++ JocDames_ProjecteMP/Moviment.cpp
@@ -1,21 +1,2 @@
 #include<iostream>
 #include"Moviment.h"
-
-void Moviment::calcularDireccion()
-{
-	if (m_origen.getPosicio()[0] < m_desti.getPosicio()[0])
-	{
-		m_direccionMovimento = 'd';
-	}
-	else if (m_origen.getPosicio()[0] > m_desti.getPosicio()[0])
-	{
-		m_direccionMovimento = 'e';
-	}
-}
-
-bool Moviment::movimentNormal()
-{
-
-
-
-}
