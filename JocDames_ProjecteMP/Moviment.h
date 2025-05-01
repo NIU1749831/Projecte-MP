d210e101c3c7e7f98dec3c09a0a347e4e8542938
Oriol <1621578@uab.cat>
2025-05-01 11:16:07 +0200

    implementacio enum classe moviment i canvi constructor

--- JocDames_ProjecteMP/Moviment.h
+++ JocDames_ProjecteMP/Moviment.h
@@ -1,5 +1,17 @@
 #include "Posicio.h"
 
+/*
+@brief: nos permite saber que tipo de movimiento es
+* tendra 3 valores: NORMAL, MATAR, MATAR_ENCADENAT, MOVIMENT_EMPTY
+*/
+
+typedef enum {
+    MOVIMENT,
+    MATAR,
+    MATAR_ENCADENAT,
+    MOVIMENT_EMPTY
+}TipusMoviment;
+
 /**
  * @class Moviment
  * @brief Classe que representa un moviment possible d'una fitxa al tauler de dames.
@@ -13,7 +25,7 @@ public:
     /**
      * @brief Constructor per defecte.
      */
-    Moviment() : m_desti("") {};
+    Moviment() : m_desti(Posicio()), m_tipusMoviment(MOVIMENT_EMPTY) {};
 
     /**
      * @brief Constructor amb parï¿½metres.
@@ -21,17 +33,20 @@ public:
      * @param desti Posiciï¿½ de destï¿½ del moviment.
      * @param pecesMortes Nombre de peces contrï¿½ries mortes en aquest moviment (opcional, per defecte 0).
      */
-    Moviment(const Posicio& desti) : m_desti(desti) {};
+    Moviment(const Posicio& desti, const TipusMoviment& tmov) : m_desti(desti), m_tipusMoviment(tmov) {};
 
     // Setters
     void setDesti(const Posicio& posicio) { m_desti = posicio; };
+    void setTipusMoviemnt(const TipusMoviment& t) { m_tipusMoviment = t; };
     
 
     //getters
     Posicio getDesti() const { return m_desti; };
+    TipusMoviment getTipusMoviemt() const { return m_tipusMoviment; };
 
  
 
 private:
-    Posicio m_desti;      // Posició de destí del moviment.
+    Posicio m_desti;                 // Posició de destí del moviment.
+    TipusMoviment m_tipusMoviment  
 };
